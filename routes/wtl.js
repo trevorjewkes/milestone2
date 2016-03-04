c34430fffb55006cb2fcfb04640d928e0a4f3e42
Trevor Jewkes <Trevor Jewkes>
2016-03-04 11:35:08 -0700

    added functions

--- routes/wtl.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET til listing. */
-router.get('/', function(req, res, next) {
-  res.render('wtl/index', { title: 'Want to Learn' });
-});
-
-module.exports = router;
