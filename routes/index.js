4673def61d639653c425eee193c0e21a90e0971a
Trevor Jewkes <Trevor Jewkes>
2016-03-25 13:53:43 -0600

    login?

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Today I Learned' });
 });
 
+router.get('/login', function(req, res, next) {
+	res.render('login', {title: 'Login - Blog' });
+});
+
+router.post('/login', function(req, res, next) {
+	var sha1sum = crypto.createHash('sha1');
+
+	req.db.driver.execQuery(
+		'SELECT * FROM users WHERE email=?;',
+		[req.body.email],
+		function(err,data){
+			if(err)
+			{
+				console.log(err);
+			}
+
+			sha1sum.update(req.body.password);
+			var hashed_inpute = sha1sum.digest('hex');
+
+			if(hashed_input == data[0].password)
+			{
+				res.cookie('username', data[0].name);
+				res.redirect('/entries/');
+			}
+			else
+			{
+				res.redirect('/login');
+			}
+		}
+	);
+
+};
+	
 module.exports = router;
