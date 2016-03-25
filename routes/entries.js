727e25c9b96e7171490c02e8eb8196ffb88343bc
Trevor Jewkes <Trevor Jewkes>
2016-03-25 11:54:03 -0600

    database

--- routes/entries.js
+++ routes/entries.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var entries = [
-  {slug:"2/13/16", body: "Today I learned how to use Lagrange multipliers in calculus.", created_at: "some date"},
-  {slug:"3/2/16", body: "Today I learned about sql", created_at: "some date"}
-];
+var entries = [];
 
 /* READ all: GET entries listing. */
 router.get('/', function(req, res, next) {
-  res.render('entries/index', { title: 'Today I Learned', entries: entries });
+  req.db.driver.execQuery(
+    "SELECT * FROM entries;",
+    function(err, data){
+      if(err){
+        console.log(err);
+      }
+
+      res.render('entries/index', { title: 'Today I Learned', entries: data });
+    }
+  );
+
 });
 
 /* CREATE entry form: GET /entries/new */
@@ -18,41 +25,116 @@ router.get('/new', function(req, res, next) {
 
 /*CREATE entry: POST /entries/ */
 router.post('/', function(req, res, next) {
-  entries.push(req.body);
-  res.render('entries/index', { title: 'Today I Learned', entries: entries });
+  req.db.driver.execQuery(
+    "INSERT INTO entries (slug,body) VALUES ('{" + req.body.slug + "}','" + req.body.body + "');",
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+    }
+  );
+
+  req.db.driver.execQuery(
+    "SELECT * FROM entries;",
+    function(err, data){
+      if(err){
+        console.log(err);
+      }
+
+      res.render('entries/index', { title: 'Today I Learned', entries: data });
+    }
+  );
 });
 
 /* UPDATE entry form: GET /entries/1/edit */
 router.get('/:id/edit', function(req, res, next) {
-  res.render('entries/update',
-  {
-    title: 'Update an entry',
-    id: req.params.id,
-    entry: entries[req.params.id]
-  });
+
+  req.db.driver.execQuery(
+    'SELECT * FROM entries WHERE id=' + parseInt(req.params.id) + ';',
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+
+      res.render('entries/update',
+      {
+        title: 'Update an entry',
+        entry: data[0]
+      });
+    }
+  );
+
 });
 
 /* UPDATE entry: POST /entries/1 */
 router.post('/:id', function(req, res, next) {
-  entries[req.params.id] = req.body;
-  res.render('entries/index',
-  {
-    title: 'Update an entry',
-    entries: entries
-  });
+  var sqlstring = "UPDATE entries SET slug='" + req.body.slug + "',body='" + req.body.body + "' WHERE id=" + parseInt(req.params.id) + ";";
+  console.log(sqlstring);
+
+  req.db.driver.execQuery(
+    sqlstring,
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+    }
+  );
+
+  req.db.driver.execQuery(
+    'SELECT * FROM entries WHERE id=' + parseInt(req.params.id) + ';',
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+
+      res.render('entries/entry', {title: "a entry", entry: data[0]});
+    }
+  );
 });
 
 /* DELETE entry: GET /entries/1/delete  */
 router.get('/:id/delete', function(req, res, next) {
-  var id = req.params.id
-  entries = entries.slice(0,id).concat(entries.slice(id+1, entries.length));
-  res.render('entries/index', { title: 'Today I Learned', entries: entries });
+  req.db.driver.execQuery(
+    'DElETE FROM entries WHERE id=' + parseInt(req.params.id) + ';',
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+    }
+  );
+
+  req.db.driver.execQuery(
+    "SELECT * FROM entries;",
+    function(err, data){
+      if(err){
+        console.log(err);
+      }
+
+      res.render('entries/index', { title: 'Today I Learned', entries: data });
+    }
+  );
 });
 
 /* THIS NEEDS TO BE LAST or /new goes here rather than where it should */
 /* READ one entry: GET /entries/0 */
 router.get('/:id', function(req, res, next) {
-  res.render('entries/entry', {title: "a entry", entry: entries[req.params.id]});
+  req.db.driver.execQuery(
+    'SELECT * FROM entries WHERE id=' + parseInt(req.params.id) + ';',
+    function(err, data){
+      if(err)
+      {
+        console.log(err);
+      }
+
+      res.render('entries/entry', {title: "a entry", entry: data[0]});
+    }
+  );
+
 });
 
 module.exports = router;
